081feb80ba01056a43e3fb64e450d7f923f4c6ce
wonseok <wonseok5893@naver.com>
2021-04-21 01:20:21 +0900

    [REFACTOR] 상품조회 return List -> ListResult<T>

--- server/shoppingmall/src/main/java/com/project/shoppingmall/controller/ProductController.java
+++ server/shoppingmall/src/main/java/com/project/shoppingmall/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.project.shoppingmall.controller;
 
 import com.project.shoppingmall.controller.requestdto.product.RequestFindAllProducts;
+import com.project.shoppingmall.controller.responsedto.common.CommonResult;
+import com.project.shoppingmall.controller.responsedto.common.ListResult;
 import com.project.shoppingmall.controller.responsedto.product.ResponseProduct;
 import com.project.shoppingmall.domain.Product;
 import com.project.shoppingmall.service.impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +20,9 @@ import java.util.List;
 public class ProductController {
 
     private final ProductServiceImpl productService;
+
     @GetMapping("")
-    public List<ResponseProduct> findAll(RequestFindAllProducts requestfindAllProducts){
-        return productService.findAllByPagingAndSorting(requestfindAllProducts);
+    public ListResult<ResponseProduct> findAllByPagingAndSorting(@RequestParam(value = "pageNum", defaultValue = "0") int pageNum, @RequestParam(value = "size", defaultValue = "10") int size, @RequestParam(value = "sortedBy") String sortedBy) {
+        return new ListResult<>(productService.findAllByPagingAndSorting(new RequestFindAllProducts(pageNum, size, sortedBy)), CommonResult.getSuccessResult());
     }
 }
