dd0aaa2a28298294fac735a720f66309b2de2194
wonseok <wonseok5893@naver.com>
2021-04-23 00:42:45 +0900

    [FIX] Object to Json 무한 Loop @JsonIgnore

--- server/shoppingmall/src/main/java/com/project/shoppingmall/domain/Category.java
+++ server/shoppingmall/src/main/java/com/project/shoppingmall/domain/Category.java
@@ -1,7 +1,9 @@
 package com.project.shoppingmall.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.project.shoppingmall.controller.requestdto.category.RequestCategoryEnrollInfo;
 import lombok.*;
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -16,13 +18,14 @@ public class Category {
     private int id;
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
     private List<Product> products = new ArrayList<>();
 
     // 생성 메서드
-    public static Category enrollCategory(RequestCategoryEnrollInfo categoryEnrollInfo){
+    public static Category enrollCategory(String name){
         Category category = new Category();
-        category.name = categoryEnrollInfo.getName();
+        category.name = name;
         return category;
     }
 
